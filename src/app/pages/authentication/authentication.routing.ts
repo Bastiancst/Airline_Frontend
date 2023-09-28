8890c7e10a04fe5925707e32c70bf22f80d21a9f
Bastiancst <121899892+Bastiancst@users.noreply.github.com>
2023-09-28 13:38:30 -0300

    Revert "Integration with crud "

--- src/app/pages/authentication/authentication.routing.ts
+++ src/app/pages/authentication/authentication.routing.ts
@@ -1,10 +1,7 @@
 import { Routes } from '@angular/router';
+
 import { AppSideLoginComponent } from './login/login.component';
 import { AppSideRegisterComponent } from './register/register.component';
-import { AppSideVerifyComponent } from './verify-code/verify-code.component';
-import { emailVerifyComponent } from './verify-email/verify-email.component';
-import { forgotPassComponent } from './forgotPass/forgotPassComponent';
-import { enterPassComponent } from './enterPass/enterPass.component';
 
 export const AuthenticationRoutes: Routes = [
   {
@@ -17,22 +14,6 @@ export const AuthenticationRoutes: Routes = [
       {
         path: 'register',
         component: AppSideRegisterComponent,
-      },
-      {
-        path: 'verify-code',
-        component: AppSideVerifyComponent
-      },
-      {
-        path: 'verify-email',
-        component: emailVerifyComponent,
-      },
-      {
-        path: 'forgotPass',
-        component: forgotPassComponent,
-      },
-      {
-        path: 'enterPass',
-        component: enterPassComponent,
       },
     ],
   },
