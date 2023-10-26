50fbcf40807f95f2dd0dae4a615598e91cf87dd2
martinmatiasz <martinmatiasz1978@gmail.com>
2023-10-26 02:22:01 -0300

    Assignment Details

--- src/app/pages/ui-components/assigment-crud/assigment-add/assigment-add/assigment-add.component.ts
+++ src/app/pages/ui-components/assigment-crud/assigment-add/assigment-add/assigment-add.component.ts
@@ -7,6 +7,7 @@ import { Validators } from '@angular/forms';
 import { CoreService } from 'src/app/pages/core/core.service';
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -18,10 +19,75 @@ export class AssigmentAddComponent {
   assignmentForm: FormGroup;
   receiverForm: FormGroup;
 
+  Origins: string[] = [
+    'Santiago, Chile',
+    'Puerto Montt, Chile',
+    'Iquique, Chile',
+    'Concepción, Chile',
+    'Buenos Aires, Argentina',
+    'Ciudad de Córdoba, Aregntina',
+    'Lima, Peru',
+    'Quito, Ecuador',
+    'Cuidad de Mexico, Mexico',
+    'Guadalajara, Mexico',
+    'Monterrey, Mexico',
+    'La Paz, Bolivia',
+    'Sao Pablo, Brazil',
+    'Rio de Janeiro, Brazil',
+    'Brasilia, Brazil',
+    'Bogotá, Colombia',
+    'Caracas, Venenzuela',
+    'San Juan, Puerto Rico',
+    'Montevideo, Uruguay',
+    'Asunción, Paraguay',
+    'San Salvador, El Salvador',
+    'Miami, E.E.U.U',
+    'Los Angeles, E.E.U.U',
+    'Miami, E.E.U.U',
+    'New York, E.E.U.U',
+    'Washinton DC, E.E.U.U',
+    'Cuidad de Guatemala, Guatemala',
+    'Tegucipalga, Honduras',
+    'Panamá, Panamá',
+  ];
+
+  Destinations: string[] = [
+    'Santiago, Chile',
+    'Puerto Montt, Chile',
+    'Iquique, Chile',
+    'Concepción, Chile',
+    'Buenos Aires, Argentina',
+    'Ciudad de Córdoba, Aregntina',
+    'Lima, Peru',
+    'Quito, Ecuador',
+    'Cuidad de Mexico, Mexico',
+    'Guadalajara, Mexico',
+    'Monterrey, Mexico',
+    'La Paz, Bolivia',
+    'Sao Pablo, Brazil',
+    'Rio de Janeiro, Brazil',
+    'Brasilia, Brazil',
+    'Bogotá, Colombia',
+    'Caracas, Venenzuela',
+    'San Juan, Puerto Rico',
+    'Montevideo, Uruguay',
+    'Asunción, Paraguay',
+    'San Salvador, El Salvador',
+    'Miami, E.E.U.U',
+    'Los Angeles, E.E.U.U',
+    'Miami, E.E.U.U',
+    'New York, E.E.U.U',
+    'Washinton DC, E.E.U.U',
+    'Cuidad de Guatemala, Guatemala',
+    'Tegucipalga, Honduras',
+    'Panamá, Panamá',
+  ];
+
   constructor(
     private _fb: FormBuilder,
     private _assignService: AssignmentService,
     private _coreService: CoreService,
+    private router:Router,
   ) {
     this.assignmentForm = this._fb.group({
       height: '',
@@ -37,7 +103,7 @@ export class AssigmentAddComponent {
       name: '',
       lastName: '',
       addres: '',
-      email: '',
+      email: ['', [Validators.required, Validators.email]],
       phoneNumber: '',
     })
   }
@@ -51,12 +117,26 @@ export class AssigmentAddComponent {
       this._assignService.addAssignment(this.assignmentForm.value).subscribe({
         next: (val: any) => {
           this._coreService.openSnackBar('Encomienda añadida!');
+          this.router.navigate(['/ui-components/assigment']);
         },
         error: (err: any) => {
           console.error(err);
         },
       });
   }
+
+  limitNumberInput(num: number, event: any) {
+    const input = event.target;
+    const maxLength = num;
+    if (input.value.length > maxLength) {
+      input.value = input.value.slice(0, maxLength);
+    }
+  }
+
+  BackToAssignmentList(){
+    this.router.navigate(['/ui-components/assigment']);
+  }
+  
 
   
    
