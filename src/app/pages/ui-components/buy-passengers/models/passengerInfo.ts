b44d78d35651f61bfb2413be204f470ec8ea214e
fcastro772 <pipahcastro7@gmail.com>
2023-10-26 12:23:32 -0300

    buy passengers crud

---
+++ src/app/pages/ui-components/buy-passengers/models/passengerInfo.ts
@@ -0,0 +1,13 @@
+export class PassengerInfo {
+    constructor(
+        public Name: string,
+        public Lastname: string,
+        public IdentityDocument: string,
+        public Age: number,
+        public Address: string,
+        public PhoneNumber: string,
+        public Email: string,
+        public SeatNumber: string,
+        public IsCopyDocumentEmail: boolean   
+    ){}
+} 
