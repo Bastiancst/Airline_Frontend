62d8288ed401b1ce1d49c6809ef4860c66117324
fcastro772 <pipahcastro7@gmail.com>
2023-11-01 18:24:51 -0300

    Update Dashboard Role

--- src/app/layouts/full/sidebar/sidebar-data.ts
+++ src/app/layouts/full/sidebar/sidebar-data.ts
@@ -3,13 +3,11 @@ import { NavItem } from './nav-item/nav-item';
 export const navItems: NavItem[] = [
   {
     navCap: 'Home',
-    requiresRole: ["Admin"]
   },
   {
     displayName: 'Dashboard',
     iconName: 'layout-dashboard',
     route: '/dashboard',
-    requiresRole: ["Admin"]
   },
   {
     navCap: 'Ui Components',
