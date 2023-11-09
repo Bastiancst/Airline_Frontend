7fe6cbbb1cbfba3ae12a18c617c2fea31e71cad1
fcastro772 <pipahcastro7@gmail.com>
2023-11-09 12:20:04 -0300

    toggle my profile icon done

--- src/app/layouts/full/header/header.component.ts
+++ src/app/layouts/full/header/header.component.ts
@@ -5,18 +5,18 @@ import {
   EventEmitter,
   Input,
   ViewEncapsulation,
+  OnInit,
 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { CoookieService } from 'src/app/services/cookie.service';
-
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   encapsulation: ViewEncapsulation.None,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
   @Input() showToggle = true;
   @Input() toggleChecked = false;
   @Output() toggleMobileNav = new EventEmitter<void>();
@@ -24,12 +24,21 @@ export class HeaderComponent {
   @Output() toggleCollapsed = new EventEmitter<void>();
 
   showFiller = false;
+  isLogged = false;
 
   constructor(public dialog: MatDialog, private CookieService: CoookieService, private _router: Router) {}
+  ngOnInit(): void {
+    this.isLoggedComp();
+  }
 
   logOut(): void{
     this.CookieService.removeCookies();
     this._router.navigate(['/authentication/login']);
+  }
+
+  isLoggedComp(){
+    if(this.CookieService.getToken()) this.isLogged = true;
+    else this.isLogged = false;
   }
 
 }
--- src/app/services/api-request.service.ts
+++ src/app/services/api-request.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class ApiRequestService {
 
-    private baseUrl: string = 'https://05cf-200-27-88-4.ngrok-free.app';
+    private baseUrl: string = 'https://12cf-200-27-88-4.ngrok-free.app';
 
     constructor(private http: HttpClient) 
     { 
